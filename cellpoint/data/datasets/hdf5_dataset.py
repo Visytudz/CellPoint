a789d8872bae1523f7935c6b4ce0549c5ad99726
ZhangGuowei <visytudz@gmail.com>
2025-12-23 21:54:13 +0800

    fix: remove unused volume data retrieval in HDF5Dataset

--- cellpoint/data/datasets/hdf5_dataset.py
+++ cellpoint/data/datasets/hdf5_dataset.py
@@ -141,18 +141,13 @@ class HDF5Dataset(data.Dataset):
 
     def __getitem__(self, item: int) -> dict[str, any]:
         """Retrieves a single data point from the dataset using lazy loading."""
-        # Get point cloud & other data
+        # Get point cloud
         if self.load_to_ram:
             pcl = self.data[item].copy().astype(np.float32)
         else:
             h5_path, index_in_file = self.points[item]
             with h5py.File(h5_path, "r") as f:
                 pcl = f["data"][index_in_file].astype(np.float32)
-                volume = (
-                    f["volume"][index_in_file].astype(np.float32)
-                    if "volume" in f
-                    else 0.0
-                )
 
         # randomly sample, normalize and convert to tensor
         if self.num_points is not None and self.num_points < pcl.shape[0]:
@@ -179,7 +174,6 @@ class HDF5Dataset(data.Dataset):
             "name": str(self.name[item]),
             "id": str(self.id[item]),
             "scale_factor": scale_factor,
-            "volume": volume,
         }
 
         return sample
